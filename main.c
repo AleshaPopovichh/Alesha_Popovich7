44f428a13723dfa2ab55130380799ac33dcbf61b
AleshaPopovichh <osenalexey2018@gmail.com>
2026-01-14 10:02:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bmp.h"
+#include "filters.h"
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Usage: %s in.bmp out.bmp\n", argv[0]);
+        return 0;
+    }
+
+    Image img;
+    if (load_bmp(argv[1], &img)) {
+        printf("ERROR: unsupported BMP format\n");
+        return 1;
+    }
+
+    save_bmp(argv[2], &img);
+    free(img.data);
+    return 0;
+}
 
